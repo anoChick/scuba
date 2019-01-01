08b2df5f42fbe57e65a00378614b5d0e3346bf0e
anoChick <anoinchick@gmail.com>
2019-01-01 17:41:38 +0900

    webhook API追加

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,36 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp()
+const firestore = admin.firestore()
+firestore.settings({
+  timestampsInSnapshots: true
+})
+const webhooksRef = firestore.collection('webhooks')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = require('cors')({ origin: true })
+exports.webhook = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const webhookID = request._parsedUrl.pathname.slice(1)
+    webhooksRef
+      .doc(webhookID)
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          const code = doc.data().code
+          try {
+            // eslint-disable-next-line no-eval
+            eval(code)
+          } catch (e) {
+            response.send(e.stack)
+          }
+        } else {
+          response.send('No such document.')
+        }
+
+        return null
+      })
+      .catch(error => {
+        response.send(error.message)
+      })
+  })
+})
