68fc6550108f6ddfe5e1495f0e14a726dd74480e
anoChick <anoinchick@gmail.com>
2019-01-02 03:26:00 +0900

    firestore.rules修正

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -1,4 +1,9 @@
 export default async function ({ store, redirect, route }) {
+  console.log(route.name)
+  if(route.name == 'forms-form-form'){
+    return
+  }
+
   if(route.name != 'index' && !store.state.currentUser) {
     redirect('/')
   }
